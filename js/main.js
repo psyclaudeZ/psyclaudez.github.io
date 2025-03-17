4bb952c2080802e489c138f71e79a3f610b6f8ae
Bowei Zhang <zbw0517@gmail.com>
2025-03-17 01:34:31 -0700

    feat: capability to create tweety posts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+const posts = [
+  {
+    title: "Yet Another Yet Another Blog",
+    date: "2025-03-17",
+    type: "Tweety",
+    content: `This is my <code>Nth</code> blog and <code>N-1</code> "Yet Another Blog" post, where <code>N >= 3</code>. Let's see how far I can go this time.`,
+    url: "",
+  },
+  // {
+  //   title: "Test blog",
+  //   date: "2025-03-17",
+  //   type: "Blog",
+  //   content: "hello",
+  //   url: "https://www.google.com",
+  // },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   const writingLink = document.getElementById("writing-link");
   const profileImage = document.getElementById("profile-image");
@@ -7,7 +24,9 @@ document.addEventListener("DOMContentLoaded", () => {
   writingLink.addEventListener("click", (e) => {
     e.preventDefault();
     aboutSection.classList.add("hidden");
+    writingSection.innerHTML = generatePostLink(posts);
     writingSection.classList.remove("hidden");
+    registerPostLink();
   });
 
   profileImage.addEventListener("click", () => {
@@ -15,3 +34,32 @@ document.addEventListener("DOMContentLoaded", () => {
     aboutSection.classList.remove("hidden");
   });
 });
+
+function generatePostLink(posts) {
+  const listItems = posts.map((post, idx) => {
+    const isTweety = post.type === "Tweety";
+    return `<li><span class="date">${
+      post.date
+    }</span> <a class="post-link" href=${isTweety ? "#" : post.url} target=${
+      isTweety ? "_self" : "_tab"
+    } data-post-id=${idx}>[${post.type}] ${post.title}</a></li>`;
+  });
+  return `<ul class="writing-list">${listItems.join("")}</ul>`;
+}
+
+function registerPostLink() {
+  const postLinks = document.querySelectorAll(".post-link");
+  postLinks.forEach((postLink) => {
+    postLink.addEventListener("click", (e) => {
+      const postId = e.target.dataset.postId;
+      const post = posts[postId];
+
+      if (!post || post.type !== "Tweety") {
+        return;
+      }
+
+      const writingSection = document.getElementById("writing-section");
+      writingSection.innerHTML = `<h3>${post.title}</h3><p>${post.content}</p><span class="date">${post.date}</span>`;
+    });
+  });
+}
